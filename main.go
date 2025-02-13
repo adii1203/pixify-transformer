8986df91c99b72614fb4387fbfec5d28bedb91f7
adii1203 <aditya32ft@gmail.com>
2025-02-13 21:51:01 +0530

    http server

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from EC2!")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port if not set
+	}
+
+	http.HandleFunc("/", helloHandler)
+
+	log.Printf("Starting server on port %s...", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatalf("Could not start server: %s", err)
+	}
 }
