71b832682c17fa913c4991756b9d52ecf61a7415
adii1203 <aditya32ft@gmail.com>
2025-02-16 22:39:47 +0530

    Enhance error logging and modify image response handling in main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"os"
 
@@ -12,12 +13,12 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file", err.Error())
 	}
 
 	s3Client, err := client.NewS3Client()
 	if err != nil {
-		log.Fatal("Error creating S3 client")
+		log.Fatal("Error creating S3 client", err.Error())
 	}
 
 	port := os.Getenv("PORT")
@@ -25,13 +26,15 @@ func main() {
 
 	e.GET("/:id", func(c echo.Context) error {
 		object, err := s3Client.GetObjectFromRawBucket(c.Param("id"))
-		object.Body.Close()
-
 		if err != nil {
 			return c.JSON(500, "Error fetching image")
 		}
 
-		return c.Stream(200, *object.ContentType, object.Body)
+		defer object.Body.Close()
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(object.Body)
+
+		return c.Stream(200, *object.ContentType, bytes.NewReader(buf.Bytes()))
 	})
 
 	e.Logger.Fatal(e.Start(":" + port))
