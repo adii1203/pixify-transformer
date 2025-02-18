ee8d5910332b1ea3b0aff46551a84dc643f40a8d
adii1203 <aditya32ft@gmail.com>
2025-02-18 15:24:09 +0530

    testing in ec2

--- main.go
+++ main.go
@@ -7,11 +7,14 @@ import (
 	"os"
 
 	client "github.com/adii1203/pixify-transformer/S3"
+	"github.com/h2non/bimg"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	v := bimg.Version
+	fmt.Println("bimg Version:", v)
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file", err.Error())
@@ -26,26 +29,25 @@ func main() {
 	e := echo.New()
 
 	e.GET("/:id", func(c echo.Context) error {
-		p := c.Path()
-		url := c.Request().URL.String()
-		fmt.Println("Path: ", p)
-		fmt.Println("URL: ", url)
-
+		fmt.Println("Request received")
 		object, err := s3Client.GetObjectFromRawBucket(c.Param("id"))
 		if err != nil {
 			return c.JSON(500, "Error fetching image")
 		}
 
 		defer object.Body.Close()
+
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(object.Body)
 
-		err = s3Client.PutObjectInProcessedBucket(c.Param("id"), buf.Bytes())
+		img, err := bimg.NewImage(buf.Bytes()).Crop(100, 100, bimg.GravityCentre)
+
+		// err = s3Client.PutObjectInProcessedBucket(c.Param("id"), buf.Bytes())
 		if err != nil {
 			return c.JSON(500, "Error saving image")
 		}
 
-		return c.Stream(200, *object.ContentType, bytes.NewReader(buf.Bytes()))
+		return c.Stream(200, *object.ContentType, bytes.NewReader(img))
 	})
 
 	e.Logger.Fatal(e.Start(":" + port))
