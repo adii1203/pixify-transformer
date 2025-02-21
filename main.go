b9a7bb72d301b76bba735bdb5dc1083d17d08344
adii1203 <aditya32ft@gmail.com>
2025-02-21 20:55:12 +0530

    Enhance image processing endpoint to support transformation options and return transformed images

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 
 	client "github.com/adii1203/pixify-transformer/S3"
+	"github.com/adii1203/pixify-transformer/transformer"
+	"github.com/adii1203/pixify-transformer/utils"
 	"github.com/h2non/bimg"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
@@ -28,7 +30,9 @@ func main() {
 	port := os.Getenv("PORT")
 	e := echo.New()
 
-	e.GET("/:id", func(c echo.Context) error {
+	e.GET("/:id/:tr", func(c echo.Context) error {
+		fmt.Println(c.Param("id"))
+		fmt.Println(c.Request().URL)
 		object, err := s3Client.GetObjectFromRawBucket(c.Param("id"))
 		if err != nil {
 			return c.JSON(500, "Error fetching image")
@@ -38,16 +42,21 @@ func main() {
 
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(object.Body)
+		m := utils.ExtractTransformationsOptions(c.Param("tr"))
+		img, err := transformer.ApplyTransformations(buf.Bytes(), m)
 
-		// img, err := bimg.NewImage(buf.Bytes()).Crop(100, 100, bimg.GravityCentre)
-
-		err = s3Client.PutObjectInProcessedBucket(c.Param("id"), buf.Bytes())
+		// err = s3Client.PutObjectInProcessedBucket(c.Param("id"), buf.Bytes())
 		if err != nil {
 			return c.JSON(500, "Error saving image")
 		}
 
-		return c.Stream(200, *object.ContentType, bytes.NewReader(buf.Bytes()))
+		return c.Stream(200, *object.ContentType, bytes.NewReader(img))
 	})
+
+	// e.GET("/:key/:tr", func(c echo.Context) error {
+	// 	m := extractTransformationsOptions(c.Param("tr"))
+	// 	return c.JSON(200, m)
+	// })
 
 	e.Logger.Fatal(e.Start(":" + port))
 }
